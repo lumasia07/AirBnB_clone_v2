25e425fe44d552806c08d8c0936bfb7e7c66948c
lumasia07 <lumasiastancelaus@gmail.com>
2024-05-06 00:42:58 -0700

    tgz

---
+++ 1-pack_web_static.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Archives contents of web_static into a tgz"""
+
+from fabric.api import local
+from datetime import datetime
+import os
+
+
+def do_pack():
+    """
+    Compresses contents of the web_static folder into an archive
+    Return:
+        Path to archive on success, otherwise None
+    """
+    if not os.path.exists("versions"):
+        os.makedirs("versions")
+
+    now = datetime.now()
+    timestamp = now.strftime("%Y%m%d%H%M%S")
+    arch_path = "versions/web_static_{}.tgz".format(timestamp)
+
+    final = local("tar -cvzf {} web_static".format(arch_path))
+
+    if final.succeeded:
+        print("web-static packed: {} -> {}Bytes".format(arch_path, os.path.getsize(arch_path)))
+        return arch_path
+    else:
+        return None
