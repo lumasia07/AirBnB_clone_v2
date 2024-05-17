9d0d0d64fa81f1b8f19c4d863b65146724e33f3f
lumasia07 <lumasiastancelaus@gmail.com>
2024-05-17 03:02:26 -0700

    number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Start a Flask Web app"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_bnb():
+    """Route to display"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_bnb():
+    """Route to display /hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    """Routes C followed by text variable"""
+    parsed_text = text.replace('_', ' ')
+    return "C {}".format(parsed_text)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def default_text(text='is cool'):
+    parsed_text = text.replace('_', ' ')
+    return "Python {}".format(parsed_text)
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number(n):
+    return "{:d} is a number".format(n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
