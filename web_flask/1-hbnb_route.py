65a0517fb6f5006aa07c5722d150dbc1a4077e92
lumasia07 <lumasiastancelaus@gmail.com>
2024-05-17 02:24:08 -0700

    fix

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,10 +10,12 @@ def hello_bnb():
     """Route to display"""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def display_bnb():
     """Route to display /hbnb"""
     return "HBNB"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
