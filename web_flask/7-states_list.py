00b1a8a14ca372079baefe9b8f3008e2ffeb6a7f
lumasia07 <lumasiastancelaus@gmail.com>
2024-05-19 12:48:06 -0700

    fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,15 +2,15 @@
 """Renders states HTML"""
 
 from flask import Flask, render_template
-from models import *
 from models import storage
+from models.state import State
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def state_list():
+def states_list():
     """Display HTML for states in alphabetical order"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 @app.teardown_appcontext
