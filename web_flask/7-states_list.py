3bad3dee7c85854bb00b636dce4d6a35de0dc0c2
lumasia07 <lumasiastancelaus@gmail.com>
2024-05-19 13:17:57 -0700

    fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,6 +4,8 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+
+
 app = Flask(__name__)
 
 
@@ -13,10 +15,12 @@ def states_list():
     states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """Closes current SQLAlchemy session"""
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
